ece0b2c201e5e75f8db3d95ef1270668fbbc6cc2
Jessica Sierra <jessica.sierraramos@gmail.com>
2022-07-01 17:30:53 -0500

    Cambiando server configuration

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
+// const jsonServer = require('json-server');
+// const auth = require('json-server-auth');
+// const middlewares = jsonServer.defaults()
+
+// const app = jsonServer.create();
+// const router = jsonServer.router(path.join(__dirname, 'db.json'));
+
+// const port = process.env.PORT || 8080;
+
+// const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
+
+// // /!\ Bind the router db to the app
+// app.db = router.db
+
+// // You must apply the auth middleware before the router
+// app.use(middlewares);
+// app.use(rules);
+// app.use(auth);
+// app.use(router);
+// app.listen(port, () => {
+//     console.log(`JSON Server is running in ${port}`);
+// });
+
 const jsonServer = require('json-server');
-const auth = require('json-server-auth');
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; // <== You can change the port
 
-const app = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+server.use(middlewares);
+server.use(router);
 
-const port = process.env.PORT || 8080;
-
-const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
-
-// /!\ Bind the router db to the app
-app.db = router.db
-
-// You must apply the auth middleware before the router
-app.use(middlewares);
-app.use(rules);
-app.use(auth);
-app.use(router);
-app.listen(port, () => {
-    console.log(`JSON Server is running in ${port}`);
-});
+server.listen(port);
