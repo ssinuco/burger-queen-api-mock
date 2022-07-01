11ea943827652e2bd34292e9438952ff7d03f884
Jessica Sierra <jessica.sierraramos@gmail.com>
2022-07-01 17:37:55 -0500

    configuracion server

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-// const fs = require('fs');
-// const path = require('path');
-// const jsonServer = require('json-server');
-// const auth = require('json-server-auth');
-// const middlewares = jsonServer.defaults()
-
-// const app = jsonServer.create();
-// const router = jsonServer.router(path.join(__dirname, 'db.json'));
-
-// const port = process.env.PORT || 8080;
-
-// const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
-
-// // /!\ Bind the router db to the app
-// app.db = router.db
-
-// // You must apply the auth middleware before the router
-// app.use(middlewares);
-// app.use(rules);
-// app.use(auth);
-// app.use(router);
-// app.listen(port, () => {
-//     console.log(`JSON Server is running in ${port}`);
-// });
-
+const fs = require('fs');
+const path = require('path');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; // <== You can change the port
+const auth = require('json-server-auth');
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
+const app = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
-server.listen(port);
+const port = process.env.PORT || 8080;
+
+const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
+
+// /!\ Bind the router db to the app
+app.db = router.db
+
+// You must apply the auth middleware before the router
+app.use(middlewares);
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port, () => {
+    console.log(`JSON Server is running in ${port}`);
+});
+
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json'); // <== Will be created later
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8080; // <== You can change the port
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
